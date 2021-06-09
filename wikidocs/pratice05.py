49bd466ae13bfd9e000a63be65feb48d18969cda
sohyunpark <sohyun@vendys.co.kr>
2021-06-09 13:15:23 +0900

    pratice05

--- wikidocs/pratice05.py
+++ wikidocs/pratice05.py
@@ -2,6 +2,7 @@ import sys
 import glob
 import time
 import random
+import os 
 
 # 05장 연습문제
 # https://wikidocs.net/42529
@@ -26,32 +27,42 @@ print("Q.01>>>{}".format(cal.value))
 
 
 #02
-class MaxLimitCalculator(Calculator):
+class MaxLimitCalculator2(Calculator):
     def add(self, val):
         self.value += val
         if self.value > 100: 
             self.value = 100 
-cal = MaxLimitCalculator()
+cal = MaxLimitCalculator2()
 cal.add(50)
 cal.add(60)
 
 print("Q.02>>>{}".format(cal.value)) 
 
+class MaxLimitCalculator(Calculator):
+    def add(self, val):
+        super().add(val)
+        if self.value > 100:
+            self.value = 100
+        
+print("Q.02>>>{}".format(cal.value)) 
+
 
 #03
-#all([1, 2, abs(-3)-3])
-#chr(ord('a')) == 'a'
-
+#all([1, 2, abs(-3)-3]) -> 
+#chr(ord('a')) == 'a' -> 아스키코드값 -> 97 -> char
+print(all([1, 2, -1]))
+print(chr(ord('a')) == 'a')
 
 #04
 a = [1, -2, 3, -5, 8, -3]
 result = filter(lambda x : x > 0, a)
+#print(result)
 print("Q.04>>>{}".format(list(result)))
 
 
 #05
 aa = hex(234)
-print("Q.05>>>{}".format(int(234)))
+print("Q.05>>>{}".format(int(aa, 16)))
 
 
 #06
@@ -79,15 +90,23 @@ d = 17 / 3
 print("Q.08>>>{}".format(round(d, 4)))
 
 
-#10
+#9
 x = 0
 # range_num = int(sys.argv[1]) + 1
 # print(range_num)
 # for i in range(1, range_num):
 #     x += i
+# print("Q.10>>>{}".format(x))
+
+print(sys.argv)
 for i in range(1, len(sys.argv)):
     x += i
 print("Q.10>>>{}".format(x))
+
+#10 
+os.chdir('/Users/sohyunpark/IdeaProjects/pythonstudy/wikidocs')
+ls = os.system('ls')
+print(ls)
 
 
 #11
@@ -108,4 +127,5 @@ while len(list) < 6:
     num = int(random.randrange(1, 45))
     if num not in list:
         list.append(num)
+list.sort()
 print("Q.13>>>{}".format(list))
